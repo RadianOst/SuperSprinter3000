dde6b6c3ec2d9e12d79bb7d617c5355e7a5afda1
Adrian <radian.ost@gmail.com>
2019-01-04 15:40:08 +0100

    delete mocked data

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { storiesArr: [
-    {
-      id: 0,
-      title: "Mama",
-      story: "Nice story",
-      criteria: "none",
-      value: 100,
-      estimations: 1,
-      status: "done"
-    },
-    {
-      id: 1,
-      title: "Papa",
-      story: "Well story",
-      criteria: "some",
-      value: 100,
-      estimations: 1,
-      status: "done"
-    }
-  ] });
+  res.render('index', { storiesArr: [] });
 });
 
 module.exports = router;
