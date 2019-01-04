6e93435709b44b0aaa2b719dcc6ff326bc93025f
Adrian <radian.ost@gmail.com>
2019-01-04 20:34:34 +0100

    add semicolons in 2 lines

--- dao/storyDAO.js
+++ dao/storyDAO.js
@@ -8,8 +8,8 @@ storyDAO.pool = new Pool()
 // the pool with emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
 storyDAO.pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
 })
 
 storyDAO.getAllStories = function(){
