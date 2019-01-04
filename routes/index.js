a81e9b557eb15619c9e57ab0fbe46d4eca4dcae3
Adrian <radian.ost@gmail.com>
2019-01-04 15:38:35 +0100

    make ejs template and some mocked data to display story list

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { storiesArr: [
+    {
+      id: 0,
+      title: "Mama",
+      story: "Nice story",
+      criteria: "none",
+      value: 100,
+      estimations: 1,
+      status: "done"
+    },
+    {
+      id: 1,
+      title: "Papa",
+      story: "Well story",
+      criteria: "some",
+      value: 100,
+      estimations: 1,
+      status: "done"
+    }
+  ] });
 });
 
 module.exports = router;
